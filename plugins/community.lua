fcfeebe87d903cb03ad682d6190be99149ab638c
rktmeister <rktmeister@rktmeister.localdomain>
2023-04-30 21:22:06 +0800

    Changed colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,6 +7,6 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.colorscheme.tokyonight" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
