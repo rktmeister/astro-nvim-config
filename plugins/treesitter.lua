88fd706ddd3c72050f8afdb9ac59e81cdeebb8e1
Dylan Raharja <rktmeister@rktmeister.localdomain>
2023-05-01 20:22:32 +0800

    Added go treesitter and lsp

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "sql", "typescript" },
+    ensure_installed = { "lua", "sql", "typescript", "go" },
   },
 }
