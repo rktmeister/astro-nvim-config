86df4b354caa8369f606160ec9d3ee416a187836
rktmeister <dylan.raharja@gmail.com>
2023-12-02 17:19:46 +0800

    Added nvim-possession to help with nvim session management

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "gennaro-tedesco/nvim-possession",
+    dependencies = {
+      "ibhagwan/fzf-lua",
+    },
+    config = true,
+    init = function()
+      local possession = require "nvim-possession"
+      vim.keymap.set("n", "<leader>sl", function() possession.list() end)
+      vim.keymap.set("n", "<leader>sn", function() possession.new() end)
+      vim.keymap.set("n", "<leader>su", function() possession.update() end)
+      vim.keymap.set("n", "<leader>sd", function() possession.delete() end)
+    end,
+  },
 }
